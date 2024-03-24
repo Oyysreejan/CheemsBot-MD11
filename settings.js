d6fa52984d60ca5d6a43a43114ab94d792d3dfca
Oyysreejan <161600696+Oyysreejan@users.noreply.github.com>
2024-03-24 11:30:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@
 //Telegram: t.me/xeonbotinc
 //GitHub: @DGXeon
 //WhatsApp: +916909137213
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/piup
 
 const fs = require('fs')
 const chalk = require('chalk')
@@ -14,24 +14,24 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT: piu" //ur yt chanel name
+global.socialm = "GitHub: Oyysreejan" //ur github or insta name
+global.location = "India, Daimond, Kolkata" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
+global.botname = '𝐐𝐮𝐞𝐞𝐧-𝐏𝐢𝐮' //ur bot name
+global.ownernumber = '919874188403' //ur owner number
+global.ownername = '𝐏𝐢𝐮' //ur owner name
+global.websitex = "https://youtu.be/sreejanedit"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.wm = "𝙋𝙞𝙪."
+global.botscript = '𝐔𝐧𝐥𝐨𝐜𝐤' //script link
+global.packname = "𝐐𝐮𝐞𝐞𝐧"
+global.author = "𝐏𝐢𝐮"
+global.creator = "917439382677@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["917439382677"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -41,7 +41,7 @@ global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
